36261cb28415f7e99475a8af6eb55267e0bf688b
Shitalendra-Immortal <shitalendrak@gmail.com>
2026-04-27 18:56:23 +0530

    Changed in code

--- game_logic/engine.py
+++ game_logic/engine.py
@@ -1,23 +1,21 @@
 import random
 
 def get_result(user_choice):
-    options = ["stone", "paper", "scissors"]
-    computer_choice = random.choice(options)
+    option = ["stone", "paper", "scissors"]
+    computer_choice = random.choice(option)
     
-    # Normalize user input
     user_choice = user_choice.lower().strip()
     
     if user_choice == computer_choice:
-        return f"It's a tie! Both chose {computer_choice.capitalize()}."
+        return f"It's a tie! Both choose {computer_choice}."
 
-    # Rules of the game
-    win_map = {
+    win= {
         "stone": "scissors",
         "paper": "stone",
         "scissors": "paper"
     }
 
-    if win_map.get(user_choice) == computer_choice:
-        return f"✨ You win! Computer chose {computer_choice.capitalize()}."
+    if win.get(user_choice) == computer_choice:
+        return f"You win! Computer chose {computer_choice}."
     else:
-        return f"🤖 Computer wins! Computer chose {computer_choice.capitalize()}."
+        return f"Computer wins! Computer chose {computer_choice}."
--- main.py
+++ main.py
@@ -11,15 +11,14 @@ def main_menu():
         choice = input("\nSelect an option (1-2): ")
         
         if choice == "1":
-            user_move = input("Enter Stone, Paper, or Scissors: ")
+            move = input("Enter Stone, Paper, or Scissors: ")
             
             
-            if user_move.lower().strip() in ["stone", "paper", "scissors"]:
-                # Call the logic from engine.py
-                result = get_result(user_move)
+            if move.lower() in ["stone", "paper", "scissors"]:
+                result = get_result(move)
                 print(f"\nRESULT: {result}")
             else:
-                print("\n❌ Invalid choice! Please type Stone, Paper, or Scissors.")
+                print("\n Invalid choice! Please type Stone, Paper, or Scissors.")
                 
         elif choice == "2":
             print("Thanks for playing! Goodbye.")
@@ -27,5 +26,4 @@ def main_menu():
         else:
             print("Invalid selection. Please choose 1 or 2.")
 
-if __name__ == "__main__":
-    main_menu()
+main_menu()
